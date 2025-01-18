cff0656f407333956f0968ca5a28e062c798cf0c
Sierra <sonicaradia@gmail.com>
2025-01-18 18:26:52 -0500

    Experimented with loops and arrays

--- js/index.js
+++ js/index.js
@@ -1,3 +1,54 @@
 // Prevent us from attempting to use variables
 // that are not declared
 "use strict"
+/*
+let array = [10, 20, 30, 40, 50];
+
+for (let i = 0; i < array.length; i++) {
+  console.log(array[i]);
+}
+
+
+let fruits = ['Apple', 'Banana', 'Mango', 'Watermelon'];
+
+for (let fruit of fruits) {
+ console.log(fruit);
+}
+
+
+let veggies = ['Broccoli', 'Lettuce', 'Cauliflower', 'Carrot', 'Onion'];
+
+veggies.forEach((string) => {
+ console.log(string);
+});
+
+
+let anArray = [1, 2, 3, 4, 5];
+
+for (let a of anArray) {
+    a = 500;
+    console.log(anArray);
+   }
+
+
+let array = [1, 2, 3, 4, 5];
+
+array.forEach(element => {
+ element = 88;
+ console.log(array);
+});
+
+
+
+let fruits = {
+    Apple: "Red",
+    Banana: "Yellow",
+    Mango: "Orange",
+    Watermelon: "Green"
+   }
+   
+   for (let fruit in fruits) {
+    console.log("Fruit: " + fruit + " Color: " + fruits[fruit]);
+   }
+
+*/
